7419d887779d8129444fa6f777f49722cfada5b7
tomeko74 <thomasz.orlowski@gmail.com>
2019-02-28 15:47:07 +0100

    fix connectionString

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-corrugated-55823',
-    user : 'postgres',
-    password : 'pass1234',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl : true,
   }
 });
 
